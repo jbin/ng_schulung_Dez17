587b470d418f78e628f5a31508b3b017aa20c817
jbin <jbin@users.noreply.github.com>
2017-12-14 11:32:57 +0100

    material

--- Stones/src/app/app.module.ts
+++ Stones/src/app/app.module.ts
@@ -4,14 +4,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductModule } from './product/product.module';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, ProductModule
+    BrowserModule, ProductModule, MatToolbarModule, MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
